deca2469c37132141f23f59872022c9ec8311f9d
shailendrasharmaab1122-tech <shailendrasharmaab1122@gmail.com>
2026-06-04 20:30:02 +0530

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -2,7 +2,8 @@ export default async function handler(req, res) {
   try {
     const { endpoint, ...params } = req.query;
     const queryString = new URLSearchParams(params).toString();
-    const response = await fetch(`https://apiserver.deltastudy.site/api/pw/${endpoint}?${queryString}`);
+    const targetUrl = `https://apiserver.deltastudy.site/api/pw/${endpoint}?${queryString}`;
+    const response = await fetch(targetUrl);
     const data = await response.json();
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200).json(data);
