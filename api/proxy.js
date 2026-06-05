e3ea160c4de52bf21669ecd0d58b6cd69fb23353
shailendrasharmaab1122-tech <shailendrasharmaab1122@gmail.com>
2026-06-05 18:54:52 +0530

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -3,8 +3,21 @@ export default async function handler(req, res) {
     const { endpoint, ...params } = req.query;
     const queryString = new URLSearchParams(params).toString();
     const targetUrl = `https://apiserver.deltastudy.site/api/pw/${endpoint}?${queryString}`;
-    const response = await fetch(targetUrl);
+
+    // Browser se aaye hue headers (Auth Token) ko capture karein
+    const authToken = req.headers['authorization'];
+
+    const response = await fetch(targetUrl, {
+      method: 'GET',
+      headers: {
+        'Authorization': authToken || '',
+        'Referer': 'https://pwthor.live/',
+        'Content-Type': 'application/json'
+      }
+    });
+
     const data = await response.json();
+    
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200).json(data);
   } catch (error) {
