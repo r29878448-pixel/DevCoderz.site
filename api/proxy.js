e31ea50983337ac4307d76454f6c8102e74b52eb
shailendrasharmaab1122-tech <shailendrasharmaab1122@gmail.com>
2026-06-06 15:26:43 +0530

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,26 +1,12 @@
 export default async function handler(req, res) {
-  try {
-    const { endpoint, ...params } = req.query;
-    const queryString = new URLSearchParams(params).toString();
-    const targetUrl = `https://apiserver.deltastudy.site/api/pw/${endpoint}?${queryString}`;
-
-    // Browser se aaye hue headers (Auth Token) ko capture karein
-    const authToken = req.headers['authorization'];
-
-    const response = await fetch(targetUrl, {
-      method: 'GET',
-      headers: {
-        'Authorization': authToken || '',
-        'Referer': 'https://pwthor.live/',
-        'Content-Type': 'application/json'
-      }
-    });
-
-    const data = await response.json();
-    
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+  const { endpoint, ...params } = req.query;
+  const targetUrl = `https://apiserver.deltastudy.site/api/pw/${endpoint}?${new URLSearchParams(params).toString()}`;
+  
+  const response = await fetch(targetUrl, {
+    headers: { 'Referer': 'https://pw.live/', 'User-Agent': 'Mozilla/5.0' }
+  });
+  
+  const data = await response.json();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).json(data);
 }
