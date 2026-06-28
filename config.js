ae97d846c7a1ae44f58858fbf9ef258ac0675891
shailendrasharmaab1122-tech <shailendrasharmaab1122@gmail.com>
2026-06-28 09:30:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
-const C = (function(_0x1a2b3c) {
-    const _0x4d5e6f = {
-        'v': _0x1a2b3c.split('').reverse().join('')
-    };
-    return _0x4d5e6f;
-})('iahbbolleh');
+const C = {
+    v: "hellobhai"
+};
